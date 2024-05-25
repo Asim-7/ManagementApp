c36009aaf0aae087bd7c5c13e530401cf6856aea
Asim <asimpsy11@gmail.com>
2024-05-25 23:14:59 +0200

    Initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 bg-slate-500 justify-center items-center">
+      <Text className="text-white text-2xl">HomeScreen</Text>
+    </View>
+  );
+}
