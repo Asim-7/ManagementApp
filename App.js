e95d2127c157f273f4680d3ec738d96c1103b38e
Asim <asimpsy11@gmail.com>
2024-05-27 21:23:19 +0200

    Naming correction done

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "./screens/LoginScreen";
-import HomeScreen from "./screens/HomeScreen";
+import MainScreen from "./screens/MainScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -16,7 +16,7 @@ export default function App() {
         />
         <Stack.Screen
           name="Home"
-          component={HomeScreen}
+          component={MainScreen}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import BottomNavigation from "../components/BottomNavigation";
 
-export default function HomeScreen({ navigation, route }) {
+export default function MainScreen({ navigation, route }) {
   return <BottomNavigation />;
 }
