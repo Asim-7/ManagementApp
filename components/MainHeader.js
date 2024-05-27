35758218f8beaec410b65d1d71e753b7b7ffcb65
Asim <asimpsy11@gmail.com>
2024-05-27 22:55:43 +0200

    Menu button added

--- components/MainHeader.js
+++ components/MainHeader.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { View, Button } from "react-native";
+import { View, Button, TouchableOpacity } from "react-native";
+import { Icon } from "react-native-paper";
 
 const Header = ({ navigation }) => {
   return (
-    <View className="flex-row justify-around p-4 bg-gray-200 mt-10">
+    <View className="flex-row justify-around items-center p-4 bg-gray-200 mt-10">
+      <TouchableOpacity onPress={() => alert("Icon clicked!")}>
+        <Icon source="menu" size={28} />
+      </TouchableOpacity>
       <Button title="Button 1" onPress={() => alert("Button 1 pressed")} />
       <Button title="Button 2" onPress={() => alert("Button 2 pressed")} />
       <Button title="Button 3" onPress={() => alert("Button 3 pressed")} />
