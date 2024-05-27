24d7140fc9bd242a80ae2c95233637e467f4c99f
Asim <asimpsy11@gmail.com>
2024-05-27 23:32:58 +0200

    alignment correction done

--- components/MainHeader.js
+++ components/MainHeader.js
@@ -5,17 +5,18 @@ import { Icon } from "react-native-paper";
 const Header = ({ navigation }) => {
   return (
     <View
-      className="flex-row justify-around items-center bg-gray-200 pb-2"
+      className="flex-row justify-between items-center bg-gray-200 pb-2 px-4"
       style={{ paddingTop: 60 }}
     >
       <Text className="text-lg">Plan X</Text>
-      <Button title="Button 2" onPress={() => alert("Button 2 pressed")} />
-      <TouchableOpacity onPress={() => alert("Account clicked!")}>
-        <Icon source="account" size={28} />
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => alert("Menu clicked!")}>
-        <Icon source="dots-vertical" size={28} />
-      </TouchableOpacity>
+      <View className="flex-row space-x-4">
+        <TouchableOpacity onPress={() => alert("Account clicked!")}>
+          <Icon source="account" size={28} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => alert("Menu clicked!")}>
+          <Icon source="dots-vertical" size={28} />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
