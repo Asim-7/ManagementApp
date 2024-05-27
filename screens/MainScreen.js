e5f8a92a8d196d0cbc607531e81638f3cfa45d04
Asim <asimpsy11@gmail.com>
2024-05-28 00:05:09 +0200

    route param fetched

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { useRoute } from "@react-navigation/native";
 import BottomNavigation from "../components/BottomNavigation";
 
-export default function MainScreen({ navigation, route }) {
+export default function MainScreen() {
+  const {
+    params: { email },
+  } = useRoute();
+
+  console.log("Item ID:", email);
+
   return <BottomNavigation />;
 }
