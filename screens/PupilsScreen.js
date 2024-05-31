8a8b95ababfe1767af6b0f5d3c8e8912de69daa0
Asim <asimpsy11@gmail.com>
2024-05-31 11:51:57 +0200

    Able to add tab layout in Pupils screen

--- screens/PupilsScreen.js
+++ screens/PupilsScreen.js
@@ -1,14 +1,35 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, useWindowDimensions } from "react-native";
 import PupilsHeader from "../components/PupilsHeader ";
+import * as React from "react";
+import { TabView, SceneMap } from "react-native-tab-view";
+import PaymentsScreen from "../screens/PaymentsScreen";
+import NotificationsScreen from "../screens/NotificationsScreen";
 
 export default function PupilsScreen() {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "first", title: "First" },
+    { key: "second", title: "Second" },
+  ]);
+
   return (
     <View className="flex-1">
       <PupilsHeader />
-      <View className="flex-1 justify-center items-center bg-slate-300">
-        <Text className="text-center text-2xl font-bold">PupilsScreen</Text>
-      </View>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={({ route, jumpTo }) => {
+          switch (route.key) {
+            case "first":
+              return <PaymentsScreen />;
+            case "second":
+              return <NotificationsScreen />;
+          }
+        }}
+        onIndexChange={setIndex}
+        initialLayout={{ width: layout.width }}
+      />
     </View>
   );
 }
