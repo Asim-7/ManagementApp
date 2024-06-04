cfd75bc877cdeabe3e4e50086bcac3f47b9fb2f0
Asim <asimpsy11@gmail.com>
2024-06-04 10:22:24 +0200

    Go back thingy added for detail screen

--- screens/main/PupilDetailsScreen.js
+++ screens/main/PupilDetailsScreen.js
@@ -1,16 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export default function PupilDetailsScreen() {
+  const navigation = useNavigation();
   const {
     params: { id, title, lesson, credit, picture },
   } = useRoute();
   return (
     <View className="flex-1 justify-center items-center bg-slate-300">
-      <Text className="text-center text-2xl font-bold">
-        PupilDetailsScreen: {title}
-      </Text>
+      <Pressable onPress={navigation.goBack}>
+        <Text className="text-center text-2xl font-bold">
+          PupilDetailsScreen: {title}
+        </Text>
+      </Pressable>
     </View>
   );
 }
