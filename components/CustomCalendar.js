3cd2bf821b52283774bc09b4ca047424fe9b990d
Asim <asimpsy11@gmail.com>
2024-06-05 23:16:49 +0200

    Agenda view added

--- components/CustomCalendar.js
+++ components/CustomCalendar.js
@@ -1,30 +1,36 @@
 import React, { useState } from "react";
-import { Calendar } from "react-native-calendars";
+import { Text, TouchableOpacity } from "react-native";
+import { Agenda } from "react-native-calendars";
 
 // Calendar reference: https://www.npmjs.com/package/react-native-calendars
 // Link: https://blog.logrocket.com/create-customizable-shareable-calendars-react-native/
 
 export default function CustomCalendar(props) {
-  const [selected, setSelected] = useState("");
-
   return (
-    <Calendar
-      onDayPress={(day) => {
-        setSelected(day.dateString);
-        console.log(`Date pressed: ${day.dateString}`);
+    <Agenda
+      selected="2024-06-12"
+      items={{
+        "2024-06-12": [
+          { name: "Cycling" },
+          { name: "Walking" },
+          { name: "Running" },
+        ],
+        "2024-06-14": [{ name: "Writing" }],
       }}
-      markedDates={{
-        [selected]: {
-          selected: true,
-          disableTouchEvent: true,
-          selectedDotColor: "orange",
-        },
-      }}
-      initialDate="2024-06-01"
-      minDate="2024-01-01"
-      maxDate="2028-01-01"
-      disableAllTouchEventsForDisabledDays={true}
-      {...props}
+      renderItem={(item, isFirst) => (
+        <TouchableOpacity
+          style={{
+            backgroundColor: "white",
+            flex: 1,
+            borderRadius: 5,
+            padding: 10,
+            marginRight: 10,
+            marginTop: 17,
+          }}
+        >
+          <Text style={{ color: "#888", fontSize: 16 }}>{item.name}</Text>
+        </TouchableOpacity>
+      )}
     />
   );
 }
